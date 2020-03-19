defa9eba15af2f8f7e6ec6480e2cd8624e2ccc4a
Jake hao <jakehao@foxmail.com>
2020-03-19 14:18:05 +0800

    static test

--- javaBasic/src/com/java/keyword/TestStatic.java
+++ javaBasic/src/com/java/keyword/TestStatic.java
@@ -1,13 +1,15 @@
 package com.java.keyword;
 
 public class TestStatic {
-    static int num;
-
+    static int num=10;
+    static {
+        System.out.println("静态代码块"+num);
+    }
     static void add() {
         num++;
     }
     public static void main(String[] args) {
         TestStatic.add();
-        System.out.println(num);
+        System.out.println("方法内"+num);
     }
 }
