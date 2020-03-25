de9136d36ca43fcd4fda828dce9b70ce93c2b43a
Jake hao <jakehao@foxmail.com>
2020-03-25 14:36:14 +0800

    日常
    测试用例

---
+++ javaBasic/src/com/java/loop/TestElseIf.java
@@ -0,0 +1,11 @@
+package com.java.loop;
+
+public class TestElseIf {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("if执行。。。。。");
+        } else if (true) {
+            System.out.println("else if执行。。。。。");
+        }
+    }
+}
